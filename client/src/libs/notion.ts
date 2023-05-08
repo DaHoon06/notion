88df5d47e54d7beddc0efc88913771212027fcec
dahoon06 <dahoon226@naver.com>
2023-05-08 23:16:22 +0900

    no message

---
+++ client/src/libs/notion.ts
@@ -0,0 +1,28 @@
+import {Client} from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_ACCESS_TOKEN,
+});
+const databaseId = process.env.NOTION_DATABASE;
+
+export const getPage = async (pageId: string) => {
+  return notion.pages.retrieve({
+    page_id: pageId
+  });
+}
+
+export const getDatabase = async () => {
+  console.log(databaseId)
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return response.results;
+};
+
+export const getBlocks = async (blockId) => {
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  return response.results;
+};
