989208f10fafc2a9c6dcd9900732963b408b9d7f
dahoon06 <dahoon226@naver.com>
2023-05-10 00:38:51 +0900

    feat: 노션 불러오기

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import {getDatabase} from "../libs/notion";
 
+interface Posts {
+
+}
+
 interface Props {
   posts: any[]
 }
+
+//TODO: Post 인터페이스 정의....?? 할수있니?
 
 const Home = (props: Props) => {
   const { posts = [] } = props;
@@ -11,9 +17,22 @@ const Home = (props: Props) => {
     <div>
       {posts.map(post => (
         <div key={post.id}>
-          <h2>{JSON.stringify(post.properties.title.title[0]?.text.content)}</h2>
+          {JSON.stringify(post.properties.title.title[0]?.text.content)} <br />
+          {JSON.stringify(post.properties.content.rich_text[0]?.text.content)} <br />
+          {JSON.stringify(post.properties.date.date?.start)} <br />
+          {JSON.stringify(post.properties.slug.rich_text[0]?.text.content)} <br />
+          {JSON.stringify(post.properties.summary.rich_text[0]?.text.content)} <br />
+          {/* TODO 태그의 경우 배열로 나옴 */}
+          {JSON.stringify(post.properties.tags.multi_select[0]?.name)}
+          {JSON.stringify(post.properties.tags.multi_select[1]?.name)}
+          {JSON.stringify(post.properties.tags.multi_select[2]?.name)}
+          <br/>
+
+          {JSON.stringify(post.properties.author)} <br />
+          {/*<h2>{JSON.stringify(post.properties.title.title[0]?.text.content)}</h2>*/}
+          {/*<hr />*/}
+          {/*<p>{JSON.stringify(post.properties.contents.rich_text[0]?.text.content)}</p>*/}
           <hr />
-          <p>{JSON.stringify(post.properties.contents.rich_text[0]?.text.content)}</p>
         </div>
       ))}
     </div>
